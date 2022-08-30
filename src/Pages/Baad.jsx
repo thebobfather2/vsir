19386e87fe78e67b1c057366a3f0ae88749b13b5
thebobfather2 <107475188+thebobfather2@users.noreply.github.com>
2022-08-29 22:51:08 -0400

    .
    Signed-off-by: thebobfather2 <107475188+thebobfather2@users.noreply.github.com>

--- src/Pages/Baad.jsx
+++ src/Pages/Baad.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import BaadLogo from "../images/BaadLogo.png";
-import DiscordGray from "../images/DiscordGray.png";
+import Discord0 from "../images/Discord0.png";
 import "./Baad.css";
 
 const Baad = () => {
   return (
   
   <div className="BaadMainContainer">
-      <h1 style={{ marginTop: "-150px" }} className="BaadTitle">Baad Goats</h1>
+      <h1 style={{ marginTop: "-200px" }} className="BaadTitle">Baad Goats</h1>
      
       <div className="BaadMain2">
       <a href="https://stake.cardinal.so/7CMdsPLuo23LXA7An2jh6At859n3PFWFFzfVAccd1wYu/">
@@ -15,19 +15,19 @@ const Baad = () => {
           className="BaadLogo"
           src={BaadLogo}
           alt="BaadLogo"
-          style={{marginTop: "40px", marginRight: "40px" }}>
+          style={{marginTop: "20px", marginRight: "40px" }}>
           </img>
-        <h3 style={{ marginTop: "-10px",  marginRight: "40px", color: "black" }}>Staking</h3>
+        <h3 style={{ marginTop: "-10px",  marginRight: "40px", color: "white" }}>Staking</h3>
       </a>
      
       <a href="https://discord.gg/GspeH23eFb/">
         <img
-          className="DiscordGray"
-          src={DiscordGray}
-          alt="DiscordGray"
-          style={{ marginTop: "40px", marginLeft: "40px" }}>
+          className="Discord0"
+          src={Discord0}
+          alt="Discord0"
+          style={{ marginTop: "20px", marginLeft: "40px" }}>
           </img>
-        <h3 style={{ marginTop: "-10px",  marginLeft: "40px", color: "black" }}>Discord</h3>
+        <h3 style={{ marginTop: "-10px",  marginLeft: "40px", color: "white" }}>Discord</h3>
       </a>
       </div>
 
