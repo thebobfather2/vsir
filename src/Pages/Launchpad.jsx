2ee950d0f06747be447c65847184a3cf8634cff2
thebobfather2 <107475188+thebobfather2@users.noreply.github.com>
2022-09-04 16:29:24 -0400

    .
    Signed-off-by: thebobfather2 <107475188+thebobfather2@users.noreply.github.com>

--- src/Pages/Launchpad.jsx
+++ src/Pages/Launchpad.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import launchimg from "../images/launchimg.png";
-import BaadLogo2 from "../images/BaadLogo.png";
+import BaadGif from "../images/baad.gif";
 import "./Launchpad.css";
 
 const Launchpad = () => {
@@ -10,16 +10,17 @@ const Launchpad = () => {
 <img className="launchimg"
           src={launchimg}
           alt="launchimg"
+          style={{marginTop: "20px"}}
           ></img>
      
       <div className="LaunchpadMain2">
       <a href="https://baadgoats.vercel.app/">
         <img
-          className="BaadLogo2"
-          src={BaadLogo2}
-          alt="BaadLogo2"
+          className="BaadGif"
+          src={BaadGif}
+          alt="BaadGif"
       ></img>
-         <h3 style={{fontSize: "2em", color: "white" }}>Mint Baad Goats</h3>
+         <h3 style={{fontSize: "1.5em", color: "white", marginTop: "10px" }}>Mint Baad Goats</h3>
       </a>
       </div>
 
