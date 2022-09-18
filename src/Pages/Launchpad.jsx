d1aecb34776b62fb40f53ecab3663a5d150ea851
thebobfather2 <107475188+thebobfather2@users.noreply.github.com>
2022-09-17 20:35:07 -0400

    updated launchpad
    Signed-off-by: thebobfather2 <107475188+thebobfather2@users.noreply.github.com>

--- src/Pages/Launchpad.jsx
+++ src/Pages/Launchpad.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import launchimg from "../images/launchimg.png";
 import BaadGif from "../images/baad.gif";
 import EddGif from "../images/EddGIF.gif";
+import CarrotGif from "../images/Carrot.gif";
 import "./Launchpad.css";
 
 const Launchpad = () => {
@@ -24,6 +25,17 @@ const Launchpad = () => {
           alt="EddGif"
       ></img>
          <h3 style={{fontSize: "1.5em", color: "white", marginTop: "10px" }}>Mint Edd Fox</h3>
+      </a>
+      </div>
+
+      <div className="LaunchpadMain2">
+      <a href="https://ghostbunny.vercel.app/">
+        <img
+          className="CarrotGif"
+          src={CarrotGif}
+          alt="CarrotGif"
+      ></img>
+         <h3 style={{fontSize: "1.5em", color: "white", marginTop: "10px" }}>Mint Ghost Bunny</h3>
       </a>
       </div>
 
